f46aa4c281dd6fcf851adfdef9e5bf0f38230a19
DamagedUnicorn <tpkzip@hotmail.com>
2022-01-31 12:39:28 +0100

    added functionality to close box adds

--- main.py
+++ main.py
@@ -12,17 +12,26 @@ import numpy as np
 import time
 import pyautogui
 
-refPlayButton = cv2.imread("/Users/thorknudsen/Desktop/playButton.png", cv2.IMREAD_GRAYSCALE)
+imgPlayButton = cv2.imread("referenceImages/playButton.png", cv2.IMREAD_GRAYSCALE)
+imgInfoAndClose = cv2.imread("referenceImages/infoAndClose.png", cv2.IMREAD_GRAYSCALE)
 
 if __name__ == '__main__':
     while True:
+        # take screenshot
         screenshot = ImageGrab.grab()
         frame = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2GRAY)
-            
-        result = cv2.matchTemplate(refPlayButton, frame, cv2.TM_CCOEFF_NORMED)
-        pos = np.unravel_index(result.argmax(), result.shape)
+        
+        # check if skip add button is in screenshot
+        #result = cv2.matchTemplate(imgPlayButton, frame, cv2.TM_CCOEFF_NORMED)
+        #if result.max() > 0.99:
+        #    pos = np.unravel_index(result.argmax(), result.shape)
+        #    pyautogui.click(int((pos[1] + 12)/2), int((pos[0] + 12)/2))
+
+        # check if close add button is in screen
+        result = cv2.matchTemplate(imgInfoAndClose, frame, cv2.TM_CCOEFF_NORMED)
         print(result.max())
         if result.max() > 0.99:
-            pyautogui.click(int(pos[1]/2), int(pos[0]/2))
+            pos = np.unravel_index(result.argmax(), result.shape)
+            pyautogui.moveTo(int((pos[1] + 15)/2), int((pos[0] + 47)/2))
 
         time.sleep(1)
